536d9c0a61a9430c00bddc2172ed5d2289744a80
Adam Smith <adam.smith@linewize.com>
2023-05-24 12:28:40 -0500

    test random

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 	"os"
 
 	"github.com/google/go-attestation/attest"
+	"github.com/google/go-tpm/tpm2"
 )
 
-func main() {
+func attestation_example() int {
 	config := &attest.OpenConfig{}
 	tpm, err := attest.OpenTPM(config)
 	if err != nil {
 		log.Fatal("Unable to open TPM")
-		os.Exit(1)
 	}
 
 	eks, err := tpm.EKs()
 	if err != nil {
 		log.Fatal("Unable to fetch EK")
-		os.Exit(1)
 	}
 	for i := 0; i < len(eks); i++ {
 		fmt.Println(eks[i].Certificate.PublicKeyAlgorithm)
@@ -30,7 +29,6 @@ func main() {
 	ak, err := tpm.NewAK(akConfig)
 	if err != nil {
 		log.Fatal("Unable to create AK")
-		os.Exit(1)
 	}
 
 	attestParams := ak.AttestationParameters()
@@ -38,22 +36,42 @@ func main() {
 	akBytes, err := ak.Marshal()
 	if err != nil {
 		log.Fatal("Unable to perform AK Marshal")
-		os.Exit(1)
 	}
 
 	if err := os.WriteFile("encrypted_ak.json", akBytes, 0600); err != nil {
 		log.Fatal("Unable to write AK")
-		os.Exit(1)
 	}
 
 	if err := os.WriteFile("ek.out", ek.Certificate.Raw, 0600); err != nil {
 		log.Fatal("Unable to write EK")
-		os.Exit(1)
 	}
 
 	if err := os.WriteFile("attest_params.out", attestParams.Public, 0600); err != nil {
 		log.Fatal("Unable to write attest Params")
-		os.Exit(1)
+	}
+	return 0
+}
+
+func rand_example() int {
+	f, err := os.OpenFile("/dev/tpmrm0", os.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("opening tpm: %v", err)
+	}
+	defer f.Close()
+
+	out, err := tpm2.GetRandom(f, 16)
+	if err != nil {
+		log.Fatalf("getting random bytes: %v", err)
 	}
 
+	fmt.Printf("%x\n", out)
+
+	return 0
+}
+
+func main() {
+	ret := 0
+	//ret = attestation_example()
+	ret = rand_example()
+	os.Exit(ret)
 }
