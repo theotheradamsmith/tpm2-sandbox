937d2bf0f5aedf86173a07cf7839ca27254738f6
Adam Smith <adam.smith@linewize.com>
2023-06-02 16:28:57 -0500

    fixup

--- main.go
+++ main.go
@@ -381,28 +381,27 @@ func generateAppK() {
 	if !ok {
 		log.Fatalf("expected ecdsa public key, got: %T", akPub)
 	}
-	fmt.Printf("akPubECDSA: %0x\n", akPubECDSA)
 
 	if len(sigData) != 64 {
 		fmt.Printf("expected ecdsa signature len 64: got %d\n", len(sigData))
 	}
 	/*
-		var r, s big.Int
-		r.SetBytes(sigData[:len(sigData)/2])
-		s.SetBytes(sigData[len(sigData)/2:])
+			var r, s big.Int
+			r.SetBytes(sigData[:len(sigData)/2])
+			s.SetBytes(sigData[len(sigData)/2:])
+
+			// Verify attested data is signed by the EK public key
+			digest := sha256.Sum256(attestData)
+			if !ecdsa.Verify(akPubECDSA, digest[:], &r, &s) {
+				log.Fatalf("signature didn't match")
+			}
 
 		// Verify attested data is signed by the EK public key
 		digest := sha256.Sum256(attestData)
-		if !ecdsa.Verify(akPubECDSA, digest[:], &r, &s) {
-			log.Fatalf("signature didn't match")
+		if !ecdsa.VerifyASN1(akPubECDSA, digest[:], sigData) {
+			log.Fatalf("VerifyASN1: signature didn't match")
 		}
 	*/
-
-	// Verify attested data is signed by the EK public key
-	digest := sha256.Sum256(attestData)
-	if !ecdsa.VerifyASN1(akPubECDSA, digest[:], sigData) {
-		log.Fatalf("VerifyASN1: signature didn't match")
-	}
 
 	// At this point the attestation data's signature is correct and can be used to
 	// further verify the application key's public key blob. Unpack the blob to
