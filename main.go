1a12c7a901c5634eac981c25e5e602298c4d7d82
Adam Smith <adam.smith@linewize.com>
2023-06-14 13:59:18 -0500

    fixup

--- main.go
+++ main.go
@@ -329,7 +329,7 @@ func generateAK() error {
 	// Generate a challenge for the name.
 	//
 	// Note that some TPMs enforce a maximum secret size of 32 bytes
-	secret := []byte("Check out my secret passphrase")
+	secret := []byte("Brevity is the soul of wit")
 	symBlockSize := 16
 	credBlob, encSecret, err := credactivation.Generate(name.Digest, ekPub, symBlockSize, secret)
 	if err != nil {
